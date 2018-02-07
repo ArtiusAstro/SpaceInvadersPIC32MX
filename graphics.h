3d55412f44d60e2528bf71280b5071c142565d9d
ArtiusAstro <aatif@kth.se>
2018-02-07 14:33:07 -0800

    how2drawPxls

---
+++ graphics.h
@@ -0,0 +1,21 @@
+#ifndef graphics_h
+#define graphics_h
+
+#include <stdint.h>
+ 
+/*A trivial for loop delay*/
+void delay(int cyc);
+/*spi send/recieve*/
+uint8_t spi_send_recv(uint8_t data);
+/*init spi*/
+void spi_init();
+/*init display*/
+void display_init();
+/*can output 4 lines of text*/
+void display_string();
+/*no clue*/
+void display_update();
+/*one pixel comes to life*/
+void pixel(int x, int y);
+ 
+#endif
