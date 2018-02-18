f8abde9e125d1d7cbee72717742e5eb6bb170c33
ArtiusAstro <28533161+ArtiusAstro@users.noreply.github.com>
2018-02-18 14:40:58 -0800

    Update graphics.h

--- graphics.h
+++ graphics.h
@@ -4,22 +4,48 @@
 
    */
 
-#ifndef graphics_h
-#define graphics_h
+#ifndef HEADER_FILE
+#define HEADER_FILE
 
-#include <stdint.h>
- 
-/*A trivial for loop delay*/
-void delay(int cyc);
-/*spi send/recieve*/
-uint8_t spi_send_recv(uint8_t data);
-/*init spi*/
-void spi_init();
-/*init display*/
-void display_init();
-/*can output 4 lines of text*/
-void display_string();
-/*no clue*/
+#include <pic32mx.h>
+#include <stdint.h> 
+
+#define DISPLAY_VDD PORTFbits.RF6
+#define DISPLAY_VBATT PORTFbits.RF5
+#define DISPLAY_COMMAND_DATA PORTFbits.RF4
+#define DISPLAY_RESET PORTGbits.RG9
+
+#define DISPLAY_VDD_PORT PORTF
+#define DISPLAY_VDD_MASK 0x40
+#define DISPLAY_VBATT_PORT PORTF
+#define DISPLAY_VBATT_MASK 0x20
+#define DISPLAY_COMMAND_DATA_PORT PORTF
+#define DISPLAY_COMMAND_DATA_MASK 0x10
+#define DISPLAY_RESET_PORT PORTG
+#define DISPLAY_RESET_MASK 0x200
+
+#define UP 1
+#define LEFT 2
+#define RIGHT 4
+#define DOWN 8
+
+typedef struct {
+        int xpos;
+        int ypos;
+} Point;
+
+void set_pixel(int, int, int);
+
+uint8_t spi_send_recv(uint8_t);
+
 void display_update();
+
+void set_disp_data();
+
+void clear_disp();
+
+void move_point(Point*);
+
+int mod(int, int);
 
 #endif
